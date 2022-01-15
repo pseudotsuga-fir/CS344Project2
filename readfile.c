b12b879789ee1e741d073bd53baae30488cbb98e
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-01-15 13:02:45 -0800

    Completed readfile.c and mycat.c

---
+++ readfile.c
@@ -0,0 +1,10 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+	int fileDesc = open(argv[1], O_RDWR);
+	char fileContents[2048];
+	int count = read(fileDesc, fileContents, 2048);
+	write(1, fileContents, count);
+	close(fileDesc);
+}
